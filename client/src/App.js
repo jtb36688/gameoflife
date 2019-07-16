a89609894dfe0e5c563f185079af1b2e286e3f0f
Jacob Bryan <jtb36688@gmail.com>
2019-07-16 16:40:39 -0400

    Simulation now working, but need to fix overlap on rows.

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,51 @@
-import React from 'react';
-import './Reset.css';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Grid from './components/Grid';
-import celldata from './defaultdata.js'
-import { Button } from 'reactstrap';
-
-
-
+import React from "react";
+import "./Reset.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Grid from "./components/Grid";
+import celldata from "./defaultdata.js";
+import { Button } from "reactstrap";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-    celldata,
-    simulating: false
-    }
+      celldata,
+      simulating: false
+    };
   }
 
   startsimulation = () => {
+    console.log(101 % 50)
     let nextstate = this.state.celldata.slice().map((cell, index) => {
-      let neighborcount = 0
+      let neighborcount = 0;
       // Check and keep count of all neighbors to particular Cell.
-      if (this.state.celldata[index-51]) {
-        neighborcount++
+      if (this.state.celldata[index - 51]) {
+        neighborcount++;
       }
-      if (this.state.celldata[index-50]) {
-        neighborcount++
+      if (this.state.celldata[index - 50]) {
+        neighborcount++;
       }
-      if (this.state.celldata[index-49]) {
-        neighborcount++
+      if (this.state.celldata[index - 49]) {
+        neighborcount++;
       }
-      if (this.state.celldata[index-1]) {
-        neighborcount++
+      if (this.state.celldata[index - 1]) {
+        neighborcount++;
       }
-      if (this.state.celldata[index+1]) {
-        neighborcount++
+      if (this.state.celldata[index + 1]) {
+        neighborcount++;
       }
-      if (this.state.celldata[index+49]) {
-        neighborcount++
+      if (this.state.celldata[index + 49]) {
+        neighborcount++;
       }
-      if (this.state.celldata[index+50]) {
-        neighborcount++
+      if (this.state.celldata[index + 50]) {
+        neighborcount++;
       }
-      if (this.state.celldata[index+51]) {
-        neighborcount++
+      if (this.state.celldata[index + 51]) {
+        neighborcount++;
       }
-
       if (this.state.celldata[index]) {
-        if (index = 479) {
-          console.log(`Cell 480 has ${neighborcount} neighbors`)
-        }
-        if ((neighborcount < 2) || (neighborcount > 3)) {
+        if (neighborcount < 2 || neighborcount > 3) {
           // Any live cell with fewer than two live neighbours dies.
           // Any live cell with more than three live neighbours dies.
           return false;
@@ -64,39 +58,42 @@ class App extends React.Component {
         if (neighborcount === 3) {
           return true;
         } else {
-          return false
+          return false;
         }
       }
-    })
-    this.setState({celldata: nextstate})
-  }
+    });
+    this.setState({ celldata: nextstate });
+  };
 
-  cellstyling = (i) => {
+  cellstyling = i => {
     if (this.state.celldata[i]) {
-      return "Cell LiveCell"
+      return "Cell LiveCell";
     } else {
-      return "Cell"
+      return "Cell";
     }
-  }
+  };
 
-  clickcell = (i) => {
-    console.log(`Cell ${i}`)
-    const changedstate = this.state.celldata.slice()
-    changedstate[i] = !changedstate[i]
+  clickcell = i => {
+    console.log(`Cell ${i}`);
+    const changedstate = this.state.celldata.slice();
+    changedstate[i] = !changedstate[i];
     this.setState({
       celldata: changedstate
-    })
-  }
+    });
+  };
 
-  render() {  
+  render() {
     return (
       <div className="App">
         <Grid cellstyling={this.cellstyling} onClick={this.clickcell} />
-        <Button onClick={() => {console.log("Simulation started")}}>Start Simulation</Button>
+        <Button
+          onClick={this.startsimulation}
+        >
+          Start Simulation
+        </Button>
       </div>
     );
-}
-
+  }
 }
 
 export default App;
